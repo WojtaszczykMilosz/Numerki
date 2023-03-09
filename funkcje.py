7202718a05f20979bf3a85c27dd776790e13db5f
Miłosz Wojtaszczyk <242567@edu.p.lodz.pl>
2023-03-09 17:24:57 +0100

    Ładnie napisane pochodne drugiego rzędu

--- funkcje.py
+++ funkcje.py
@@ -7,17 +7,26 @@ def cosinus(x):
 def cosinus_pochodna(x):
     return (-2 * np.sin(2 * x)) - 1.5
 
+def cosinus_pochodna2(x):
+    return -4 * np.cos(2 * x)
+
 def wykładnicza(x):
     return 2 ** x - 6
 
 def wykładnicza_pochodna(x):
     return np.log(2) * 2 ** x
 
+def wykładnicza_pochodna2(x):
+    return (np.log(2)**2) * (2 ** x)
+
 def złożenie(x):
     return 3 ** np.sin(x) - 2
 
 def złożenie_pochodna(x):
     return np.log(3) * (3 ** np.sin(x)) * np.cos(x)
+
+def złożenie_pochodna2(x):
+    return (np.log(3) ** 2) * (3 ** np.sin(x)) * (np.cos(x) ** 2) - np.log(3) * (3 ** np.sin(x)) * np.sin(x)
 
 def wielomian(x, tab):
     return sk.Obliczenia.horner(x,tab)
@@ -28,4 +37,3 @@ def wielomian_pochodna(x,tab):
         pochodna.append(tab[i] * (len(tab) - i - 1))
         print(pochodna)
     return  sk.Obliczenia.horner(x,pochodna)
-
