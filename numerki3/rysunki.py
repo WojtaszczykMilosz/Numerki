b066b933dd99e474bdf89d357dbb2f8a8ff989f2
242544 Szczepanek Filip <242544@edu.p.lodz.pl>
2023-04-13 17:54:02 +0200

    numerki3 rysunki

--- numerki3/rysunki.py
+++ numerki3/rysunki.py
@@ -6,17 +6,17 @@ import numpy as np
 def wykres_funkcji(funkcja, a, b, iloscWezlow):
 
     x = inter.ObliczWezlyCzebyszewa(a, b, iloscWezlow)
-    print(a)
-    print(b)
     y = funkcja(x)
 
     Lagrange = inter.InterpolacjaLagrange(x, y)
     interpolacja = Lagrange.ObliczWartoscInterpolacji
 
 
-    help = np.linspace(a, b, 500)
+
+    help = np.linspace(a, b, 5000)
     wartoscifunkcji = funkcja(help)
     wartosciInterpolacji = interpolacja(help)
+
 
     zero = np.zeros((len(help)))
     plt.figure(figsize=(10, 6))
@@ -25,8 +25,8 @@ def wykres_funkcji(funkcja, a, b, iloscWezlow):
     plt.plot(help, wartoscifunkcji,label='zadana funkcja')
     plt.plot(help, wartosciInterpolacji, color='green', label='interpolacja')
 
-
     plt.plot(x, y, 'rd', label='węzły')
+
     plt.plot(help, zero, color='black')
     plt.xlabel("OŚ X")
     plt.ylabel("OŚ Y")
